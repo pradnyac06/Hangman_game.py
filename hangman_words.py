239dc8731621aa693c69bdf727222786b950b874
Pradnya Chute <pradnya.chute@gmail.com>
2025-05-02 15:19:02 +0530

    first commit

---
+++ hangman_words.py
@@ -0,0 +1,10 @@
+word_list = ["apple", "tiger", "house", "chair", "smile", "bread", "dance", "beach", "music", "lemon",
+    "pizza", "grass", "cloud", "mango", "honey", "table", "light", "flower", "dream", "stars",
+    
+    # Medium Words
+    "journey", "kingdom", "blanket", "thunder", "pancake", "lantern", "fortune", "cartoon", "peacock", "rainbow",
+    "glasses", "cottage", "harvest", "battery", "monster", "fashion", "kitchen", "captain", "melody",
+    
+    # Hard Words
+    "astronaut", "crocodile", "whirlwind", "knowledge", "nightmare", "labyrinth", "avalanche", "chandelier", "telescope", "adventure",
+    "umbrella", "submarine", "blueprint", "keyboard", "marathon", "recycling", "wallpaper", "strawberry", "skyscraper", "microscope"]
